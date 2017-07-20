eddca545a49d9c958223fb5540041c46c8fd8d7f
Manu Gupta <manu.mgsoft@gmail.com>
2017-07-20 16:27:10 +0530

    Initial commit

---
+++ app/src/main/java/com/mgsoftcorp/firstnewapp/MainActivity.java
@@ -0,0 +1,70 @@
+package com.mgsoftcorp.firstnewapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mgsoftcorp.firstnewapp.editing.MyTextUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "First TAG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        doIt();
+    }
+
+    private void doIt() {
+        MyTextUtil util = new MyTextUtil("Manu");
+        util.doSomething();
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "doIt: Doing It!");
+        TextView tv = (TextView) findViewById(R.id.myTextView);
+        tv.setText("MyText");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/mgsoftcorp/firstnewapp/editing/MyTextUtil.java
@@ -0,0 +1,18 @@
+package com.mgsoftcorp.firstnewapp.editing;
+
+
+
+public class MyTextUtil {
+    private String name;
+
+    public MyTextUtil(String name) {
+        this.name = name;
+    }
+
+    public MyTextUtil() {
+    }
+
+    public void doSomething() {
+
+    }
+}
